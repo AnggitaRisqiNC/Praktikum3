018ca381506a6d5a73cf74183aa607259e9a2a20
AnggitaRisqiNC <anggitarisqi0907@gmail.com>
2022-10-27 10:25:20 +0700

    Menambahkan Latihan

---
+++ Latihan/Latihan 2.py
@@ -0,0 +1,11 @@
+a=input("Masukkan nilai a: ")
+b=input("Masukkan nilai b: ")
+print("Variabel a=", a)
+print("Variabel b=", b)
+str(print("Hasil Penggabungan a&b= ",a+b))
+
+# konversi nilai
+a=int(a)
+b=int(b)
+str(print("Hasil Penjumlahan a+b= ",a+b))
+str(print("Hasil Pembagian a/b= ",a/b))
